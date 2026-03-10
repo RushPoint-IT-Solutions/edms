27bb7b20d64ab0f11a994f2c34bd5d9980153ae8
Mjayy <riemazoey14@gmail.com>
2026-03-11 02:34:04 +0800

    fix the layout, display controllers, and etc. of Roles in settings

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -1,9 +1,9 @@
 <div class="modal fade" id="edit{{ $role->id }}" tabindex="-1" aria-labelledby="newModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header p-3">
-                <h5 class="modal-title" id="newModalLabel">Add new role</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" id="addFolderBtn-close" aria-label="Close"></button>
+            <div class="modal-header border-bottom border-2">
+                <h5 class="modal-title mb-3">Edit Role</h5>
+                <button type="button" class="btn-close mb-3" data-bs-dismiss="modal"></button>
             </div>
             <form autocomplete="off" method="POST" action="{{ url('/roles/update/'.$role->id) }}" onsubmit="show()">
                 @csrf
@@ -19,11 +19,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button class="btn btn-light" type="button" data-bs-dismiss="modal">
-                        Close
-                    </button>
-                    <button class="btn btn-primary" type="submit">Update</button>
+                <div class="modal-footer border-top border-2">
+                    <button type="button" class="btn btn-secondary mt-3" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary mt-3">Update</button>
                 </div>
             </form>
         </div>
--- resources/views/roles/view.blade.php
+++ resources/views/roles/view.blade.php
@@ -1,24 +1,27 @@
-<div class="modal" id="view{{ $role->id }}">
+<div class="modal fade" id="view{{ $role->id }}" tabindex="-1">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header">
-                <h6 class="modal-title">Add permissions</h6>
+            <div class="modal-header border-bottom border-2">
+                <h5 class="modal-title">Permissions — {{ $role->name }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
-            <form method="POST" action="{{ url('/roles/add-permission/'.$role->id) }}">
+            <form method="POST" action="{{ url('/roles/add-permission/'.$role->id) }}" onsubmit="show();">
                 @csrf
                 <div class="modal-body">
-                    @php
-                        $rolePermission = $role->permissions->pluck('id')->toArray();
-                    @endphp
-                    @foreach ($permissions as $permission)
-                    <div class="form-check notification-check">
-                        <input class="form-check-input" type="checkbox" name="permission[]" value="{{ $permission->name }}" id="{{ $permission->name.$role->id }}" @if(in_array($permission->id, $rolePermission)) checked @endif>
-                        <label class="form-check-label" for="{{ $permission->name.$role->id }}">{{ $permission->name }}</label>
+                    @php $rolePermission = $role->permissions->pluck('id')->toArray(); @endphp
+                    <div class="row">
+                        @foreach($permissions as $permission)
+                        <div class="col-md-4">
+                            <div class="form-check mb-2">
+                                <input class="form-check-input" type="checkbox" name="permission[]" value="{{ $permission->name }}" id="{{ $permission->name.$role->id }}" @if(in_array($permission->id, $rolePermission)) checked @endif>
+                                <label class="form-check-label" for="{{ $permission->name.$role->id }}">{{ $permission->name }}</label>
+                            </div>
+                        </div>
+                        @endforeach
                     </div>
-                    @endforeach
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                <div class="modal-footer border-top border-2">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Save</button>
                 </div>
             </form>
