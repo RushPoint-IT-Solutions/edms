7c01c1b0a8f90611183d43b9bae7a7358bd57fa1
Schultz Henry Boado <schultzxhenry@gmail.com>
2026-03-17 21:54:23 -0700

    Done see user that has access in document in share documents feature

--- resources/views/documents/share-documents.blade.php
+++ resources/views/documents/share-documents.blade.php
@@ -11,22 +11,38 @@
                 <div class="modal-body">
                     <div class="row g-3">
                         <div class="col-md-12">
+                            <label for="" class="form-label">Documents</label>
+                            <select name="documents" class="cat form-control" id="shareDocument" required>
+                                <option value=""></option>
+                                @foreach ($documents as $document)
+                                <option value="{{ $document->id }}">{{$document->control_code." ".$document->title}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col-md-12">
                             <label for="" class="form-label">Users</label>
-                            <select name="users[]" class="cat form-control" multiple required>
+                            <select name="users[]" id="userMultiple" class="cat form-control" multiple required>
                                 <option value=""></option>
                                 @foreach ($users as $user)
                                     <option value="{{ $user->id }}">{{$user->name}}</option>
                                 @endforeach
                             </select>
                         </div>
-                        <div class="col-md-12">
-                            <label for="" class="form-label">Documents</label>
-                            <select name="documents" class="cat form-control" required>
-                                <option value=""></option>
-                                @foreach ($documents as $document)
-                                <option value="{{ $document->id }}">{{$document->control_code." ".$document->title}}</option>
-                                @endforeach
-                            </select>
+                        <hr>
+                        
+                        <div id="peopleAccessContainer" style="display: none;">
+                            <p class="fs-12 m-0 mb-2">People with access</p
+                            {{-- <a href="javascript:void(0);" class="list-group-item list-group-item-action active">
+                                <div class="d-flex mb-2 align-items-center">
+                                    <div class="flex-shrink-0">
+                                        <img src="{{ asset("images/no_image.png") }}" alt="" class="avatar-sm rounded-circle" />
+                                    </div>
+                                    <div class="flex-grow-1 ms-3">
+                                        <h5 class="list-title fs-15 mb-1 text-dark" id="Name"></h5>
+                                        <p class="list-text mb-0 fs-12 text-dark" id="Email"></p>
+                                    </div>
+                                </div>
+                            </a> --}}
                         </div>
                     </div>
                 </div>
