1b8874c8a36d95d5c1c203f42e7fbaaf2f6dbbba
Mjayy <riemazoey14@gmail.com>
2026-03-27 12:19:32 +0800

    fix error

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -1289,6 +1289,9 @@ class DocumentController extends Controller
         $teams = Team::where('status', 1)->orderBy('name', 'asc')->get();
 
         $existingDocuments = Document::with('document_type_list', 'document_tags')
+            ->when(auth()->user()->role != 'Administrator', function ($q) {
+                $q->where('user_id', auth()->user()->id);
+            })
             ->selectRaw('
                 MAX(id) as id,
                 control_code,
