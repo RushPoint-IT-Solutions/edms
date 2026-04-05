7617f9e66d97cb38c4392d337f1ed71c682f2d08
Mjayy <riemazoey14@gmail.com>
2026-04-05 17:53:19 +0800

    alignment design for consistency

--- resources/views/monitoring.blade.php
+++ resources/views/monitoring.blade.php
@@ -223,7 +223,7 @@
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header border-bottom">
-                <h3 class="modal-title fw-semibold" id="vatTitle"></h3>
+                <h5 class="modal-title fw-semibold" id="vatTitle"></h5>
                 <button type="button" class="btn-close mb-2" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body" id="vatBody"></div>
