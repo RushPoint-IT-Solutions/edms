c5cb25b78396e3d7fe1154fb243dd7e487bf9fd9
Mjayy <riemazoey14@gmail.com>
2026-04-06 23:20:21 +0800

    alignment design for consistency

--- resources/views/permits/edit_type.blade.php
+++ resources/views/permits/edit_type.blade.php
@@ -1,20 +1,20 @@
 <div class="modal fade" id="changeType{{$permit->id}}" tabindex="-1">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-dialog-centered modal-md">
         <div class="modal-content">
-            <div class="modal-header border-bottom border-2">
-                <h5 class="modal-title">Change Type</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            <div class="modal-header border-bottom">
+                <h6 class="modal-title fw-semibold mb-2">Change Type</h6>
+                <button type="button" class="btn-close mb-2" data-bs-dismiss="modal"></button>
             </div>
             <form method="POST" action="{{ url('permits/change-type/'.$permit->id) }}" onsubmit="show();" enctype="multipart/form-data">
                 @csrf
                 <div class="modal-body">
                     <div class="mb-3">
-                        <label class="form-label">Title <span class="text-danger">*</span></label>
-                        <input type="text" name="title" value="{{ $permit->title }}" class="form-control" required>
+                        <label class="form-label form-label-sm">Title <span class="text-danger">*</span></label>
+                        <input type="text" name="title" value="{{ $permit->title }}" class="form-control form-control-sm" required>
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label">Type <span class="text-danger">*</span></label>
-                        <select name="type" class="form-select" required>
+                    <div class="mb-0">
+                        <label class="form-label form-label-sm">Type <span class="text-danger">*</span></label>
+                        <select name="type" class="form-select form-select-sm" required>
                             <option value="">Select type</option>
                             <option value="License" @if($permit->type == 'License') selected @endif>License</option>
                             <option value="Permit" @if($permit->type == 'Permit') selected @endif>Permit</option>
@@ -22,9 +22,9 @@
                         </select>
                     </div>
                 </div>
-                <div class="modal-footer border-top border-2">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                <div class="modal-footer border-top">
+                    <button type="button" class="btn btn-sm btn-secondary mt-3" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-sm btn-primary mt-3">Submit</button>
                 </div>
             </form>
         </div>
